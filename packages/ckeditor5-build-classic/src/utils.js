c1d40c06f36c82b3fc320676a2b18da646a15029
cyzonetech <cyzonetech@cyzone.cn>
2021-02-21 14:57:00 +0800

    普通链接转为base64形式，可粘贴上传

---
+++ packages/ckeditor5-build-classic/src/utils.js
@@ -0,0 +1,11 @@
+function isLocalImage(node) {
+	if (!node.is('element', 'img') || !node.getAttribute('src')) {
+		return false;
+	}
+	// 普通链接转为base64形式
+	if (new RegExp('^https?', 'i').test(node.getAttribute('src'))) {
+		node._setAttribute('src', 'data:image/jpeg;base64,' + btoa(node.getAttribute('src')));
+	}
+	return node.getAttribute('src').match(/^data:image\/\w+;base64,/g) ||
+		node.getAttribute('src').match(/^blob:/g);
+}
